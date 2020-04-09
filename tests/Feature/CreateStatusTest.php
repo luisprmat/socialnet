d0d8c62757203a273aacb00486ec09736fbdb792
Luis Antonio Parrado <luisprmat@gmail.com>
2020-04-09 02:22:45 -0500

    Add first tests with PHPUnit

---
+++ tests/Feature/CreateStatusTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateStatusTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function guest_users_cannot_create_statuses()
+    {
+        $response = $this->post(route('status.store'), ['body' => 'Mi primer estado']);
+
+        $response->assertRedirect('login');
+    }
+
+    /** @test  */
+    public function an_authenticated_user_can_create_statuses()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('status.store'), [
+            'body' => 'Mi primer estado'
+        ]);
+
+        $response->assertJson([
+            'body' => 'Mi primer estado'
+        ]);
+
+        $this->assertDatabaseHas('statuses', [
+            'user_id' => $user->id,
+            'body' => 'Mi primer estado'
+        ]);
+    }
+
+    /** @test */
+    function a_status_requires_a_body()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('status.store'), ['body' => '']);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'errors' => ['body']
+        ]);
+    }
+
+    /** @test */
+    function a_status_body_requires_a_minimun_length()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('status.store'), ['body' => 'asdf']);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'errors' => ['body']
+        ]);
+    }
+}
