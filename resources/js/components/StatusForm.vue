fb37587564aad2db0e39c5d0104944e2bfa32dbe
Luis Antonio Parrado <luisprmat@gmail.com>
2020-04-09 21:04:53 -0500

    Use mixins to get the authenticated user through Vue.js

--- resources/js/components/StatusForm.vue
+++ resources/js/components/StatusForm.vue
@@ -1,34 +1,41 @@
 <template>
     <div>
-        <form @submit.prevent="submit">
+        <form @submit.prevent="submit" v-if="isAuthenticated">
             <div class="card-body bg-light">
-                <textarea v-model="body" class="form-control border-0 bg-light" name="body" placeholder="¿Qué estás pensando Luis?"></textarea>
+                <textarea v-model="body"
+                        class="form-control border-0 bg-light"
+                        name="body"
+                        :placeholder="`¿Qué estás pensando ${currentUser.name}?`"
+                ></textarea>
             </div>
             <div class="card-footer">
                 <button class="btn btn-primary" id="create-status">Publicar</button>
             </div>
         </form>
+        <div v-else class="card-body">
+            <a href="/login">Debes hacer login</a>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            body: '',
-        }
-    },
-    methods: {
-        submit() {
-            axios.post('/statuses', {body: this.body})
-                .then(res => {
-                    EventBus.$emit('status-created', res.data.data);
-                    this.body = ''
-                })
-                .catch(err => {
-                    console.log(err.response.data)
-                })
+    export default {
+        data() {
+            return {
+                body: '',
+            }
+        },
+        methods: {
+            submit() {
+                axios.post('/statuses', {body: this.body})
+                    .then(res => {
+                        EventBus.$emit('status-created', res.data.data);
+                        this.body = ''
+                    })
+                    .catch(err => {
+                        console.log(err.response.data)
+                    })
+            }
         }
     }
-}
 </script>
