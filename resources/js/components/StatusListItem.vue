8adfb4d12b1bec9819c0bd7cac4fb07118bbddac
Luis Antonio Parrado <luisprmat@gmail.com>
2020-04-11 03:57:17 -0500

    Reuse like button component

--- resources/js/components/StatusListItem.vue
+++ resources/js/components/StatusListItem.vue
@@ -13,7 +13,9 @@
         <div class="card-footer p-2 d-flex justify-content-between align-items-center">
 
             <like-btn
-                :status="status"
+                dusk="like-btn"
+                :url="`/statuses/${status.id}/likes`"
+                :model="status"
             ></like-btn>
 
             <div class="text-secondary mr-2">
@@ -32,8 +34,13 @@
                     </div>
                 </div>
                 <span dusk="comment-likes-count">{{ comment.likes_count }}</span>
-                <button v-if="comment.is_liked" dusk="comment-unlike-btn" @click="unlikeComment(comment)">TE GUSTA</button>
-                <button v-else dusk="comment-like-btn" @click="likeComment(comment)">ME GUSTA</button>
+
+                <like-btn
+                    dusk="comment-like-btn"
+                    :url="`/comments/${comment.id}/likes`"
+                    :model="comment"
+                ></like-btn>
+
             </div>
 
             <form @submit.prevent="addComment" v-if="isAuthenticated">
@@ -87,26 +94,6 @@
                         console.log(err.response.data)
                     })
             },
-            likeComment(comment) {
-                axios.post(`/comments/${comment.id}/likes`)
-                    .then(res => {
-                        comment.likes_count++;
-                        comment.is_liked = true;
-                    })
-                    .catch(err => {
-                        console.log(err.response.data)
-                    })
-            },
-            unlikeComment(comment) {
-                axios.delete(`/comments/${comment.id}/likes`)
-                    .then(res => {
-                        comment.likes_count--;
-                        comment.is_liked = false;
-                    })
-                    .catch(err => {
-                        console.log(err.response.data)
-                    })
-            }
         }
     }
 </script>
