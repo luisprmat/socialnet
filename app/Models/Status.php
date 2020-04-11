652e20d03e89b7557c9bd0a1ea82ae08f8cd6aaf
Luis Antonio Parrado <luisprmat@gmail.com>
2020-04-11 01:02:21 -0500

    Reuse code adding HasLikes trait

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use App\User;
+use App\Traits\HasLikes;
 use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    use HasLikes;
+
     protected $guarded = [];
 
     public function user()
@@ -14,37 +17,8 @@ class Status extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function likes()
-    {
-        return $this->morphMany(Like::class, 'likeable');
-    }
-
     public function comments()
     {
         return $this->hasMany(Comment::class);
-    }
-
-    public function like()
-    {
-        $this->likes()->firstOrCreate([
-            'user_id' => auth()->id()
-        ]);
-    }
-
-    public function unlike()
-    {
-        $this->likes()->where([
-            'user_id' => auth()->id()
-        ])->delete();
-    }
-
-    public function isLiked()
-    {
-        return $this->likes()->where('user_id', auth()->id())->exists();
-    }
-
-    public function likesCount()
-    {
-        return $this->likes()->count();
     }
 }
