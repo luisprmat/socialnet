792e781265d607e506e996463928de539494e433
Luis Antonio Parrado <luisprmat@gmail.com>
2020-04-14 16:04:29 -0500

    Extract Comments List to a new Vue component

--- resources/js/components/StatusListItem.vue
+++ resources/js/components/StatusListItem.vue
@@ -24,31 +24,10 @@
             </div>
         </div>
         <div class="card-footer">
-            <div v-for="comment in comments" class="mb-3">
-				<div class="d-flex">
-					<img width="34px" height="34px" class="rounded shadow-sm mr-2" :src="comment.user.avatar" :alt="comment.user.name">
-					<div class="flex-grow-1">
-						<div class="card border-0 shadow-sm">
-							<div class="card-body p-2 text-secondary">
-								<a :href="comment.user.link"><strong>{{ comment.user.name }}</strong></a>
-								{{ comment.body }}
-							</div>
-						</div>
-						<small class="badge badge-pill badge-primary py-1 px-2 mt-1 float-right" dusk="comment-likes-count">
-							<i class="fas fa-thumbs-up fa-fw"></i>
-							{{ comment.likes_count }}
-						</small>
-
-						<like-btn
-							dusk="comment-like-btn"
-							:url="`/comments/${comment.id}/likes`"
-							:model="comment"
-							class="comments-like-btn"
-						></like-btn>
-					</div>
-				</div>
-            </div>
-
+            <comment-list
+                :comments="status.comments"
+                :status-id="status.id"
+            ></comment-list>
             <form @submit.prevent="addComment" v-if="isAuthenticated">
                 <div class="d-flex align-items-center">
                     <img class="rounded shadow-sm mr-2" width="34px"
@@ -74,6 +53,7 @@
 
 <script>
     import LikeBtn from './LikeBtn';
+    import CommentList from './CommentList';
 
     export default {
         props: {
@@ -85,21 +65,15 @@
         data() {
             return {
                 newComment: '',
-                comments: this.status.comments
             }
         },
-        mounted() {
-            Echo.channel(`statuses.${this.status.id}.comments`).listen('CommentCreated', ({comment}) => {
-                this.comments.push(comment);
-            })
-        },
-        components: { LikeBtn },
+        components: { LikeBtn, CommentList },
         methods: {
             addComment() {
                 axios.post(`/statuses/${this.status.id}/comments`, {body: this.newComment})
                     .then(res => {
                         this.newComment = '';
-                        this.comments.push(res.data.data);
+                        EventBus.$emit(`statuses.${this.status.id}.comments`, res.data.data);
                     })
                     .catch(err => {
                         console.log(err.response.data)
