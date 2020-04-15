f0ecdbfe12be1967b8e77b05ca8892d4b6842f94
Luis Antonio Parrado <luisprmat@gmail.com>
2020-04-14 21:29:08 -0500

    Add likes and unlikes in real time to comments

--- resources/js/components/CommentList.vue
+++ resources/js/components/CommentList.vue
@@ -1,37 +1,19 @@
 <template>
     <div>
-        <div v-for="comment in comments" class="mb-3">
-            <div class="d-flex">
-                <img width="34px" height="34px" class="rounded shadow-sm mr-2" :src="comment.user.avatar" :alt="comment.user.name">
-                <div class="flex-grow-1">
-                    <div class="card border-0 shadow-sm">
-                        <div class="card-body p-2 text-secondary">
-                            <a :href="comment.user.link"><strong>{{ comment.user.name }}</strong></a>
-                            {{ comment.body }}
-                        </div>
-                    </div>
-                    <small class="badge badge-pill badge-primary py-1 px-2 mt-1 float-right" dusk="comment-likes-count">
-                        <i class="fas fa-thumbs-up fa-fw"></i>
-                        {{ comment.likes_count }}
-                    </small>
-
-                    <like-btn
-                        dusk="comment-like-btn"
-                        :url="`/comments/${comment.id}/likes`"
-                        :model="comment"
-                        class="comments-like-btn"
-                    ></like-btn>
-                </div>
-            </div>
-        </div>
+        <comment-list-item
+            v-for="comment in comments"
+            :comment="comment"
+            :key="comment.id"
+            class="mb-3"
+        ></comment-list-item>
     </div>
 </template>
 
 <script>
-    import LikeBtn from './LikeBtn';
+    import CommentListItem from './CommentListItem';
 
     export default {
-        components: { LikeBtn },
+        components: { CommentListItem },
         props: {
             comments: {
                 type: Array,
---
+++ resources/js/components/CommentListItem.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="d-flex">
+        <img width="34px" height="34px" class="rounded shadow-sm mr-2" :src="comment.user.avatar" :alt="comment.user.name">
+        <div class="flex-grow-1">
+            <div class="card border-0 shadow-sm">
+                <div class="card-body p-2 text-secondary">
+                    <a :href="comment.user.link"><strong>{{ comment.user.name }}</strong></a>
+                    {{ comment.body }}
+                </div>
+            </div>
+            <small class="badge badge-pill badge-primary py-1 px-2 mt-1 float-right" dusk="comment-likes-count">
+                <i class="fas fa-thumbs-up fa-fw"></i>
+                {{ comment.likes_count }}
+            </small>
+
+            <like-btn
+                dusk="comment-like-btn"
+                :url="`/comments/${comment.id}/likes`"
+                :model="comment"
+                class="comments-like-btn"
+            ></like-btn>
+        </div>
+    </div>
+</template>
+
+<script>
+    import LikeBtn from './LikeBtn';
+
+    export default {
+        components: { LikeBtn },
+        props: {
+            comment: {
+                type: Object,
+                required: true
+            }
+        },
+        mounted() {
+            Echo.channel(`comments.${this.comment.id}.likes`)
+                .listen('ModelLiked', comment => {
+                    this.comment.likes_count++;
+                });
+
+            Echo.channel(`comments.${this.comment.id}.likes`)
+                .listen('ModelUnliked', comment => {
+                    this.comment.likes_count--;
+                });
+        }
+    }
+</script>
